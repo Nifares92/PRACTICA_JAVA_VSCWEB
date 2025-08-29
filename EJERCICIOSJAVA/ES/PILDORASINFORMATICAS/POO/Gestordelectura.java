9cf5acdaaa0b564b503c653c8e4fc50182b718ec
Nifares92 <serabohe@gmail.com>
2025-08-29 11:52:47 +0000

    Tu mensaje de commit

---
+++ EJERCICIOSJAVA/ES/PILDORASINFORMATICAS/POO/Gestordelectura.java
@@ -0,0 +1,26 @@
+package EJERCICIOSJAVA.ES.PILDORASINFORMATICAS.POO;
+
+public class Gestordelectura {
+
+//Atributo.
+//Primero vamos a señalar/fijar los atributos del gestor de lecturas, que solo se van a poder modificar desde esta clase.
+
+private String nombrelector;
+
+//CONSTRUCTOR. Ahora vamos a por el constructor.
+//Podemos entender el constructor como el esqueleto del objeto que vamos a crear a posteriori en nuestra clase principal. En este caso, llamaremos a este constructor cuando se cumpla una condidicón: String nombrelector.
+
+public Gestordelectura (String nombrelector){
+
+this.nombrelector=nombrelector;
+
+}
+
+//Métodos. En este caso, los métodos son las acciones que llevaremos a cabo, y que interactuarán con tanto nuestra clase princpial, como el resto de clases de este código para poder generar en la principal el resultado que esperamos.
+
+
+
+
+
+
+}
